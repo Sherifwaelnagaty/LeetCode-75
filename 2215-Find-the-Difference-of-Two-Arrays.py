44627ff7472224779ff5c09213e0018e0335b959
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2024-04-03 03:37:14 +0200

    [2215] [Time Beats: 49.77%] [Memory Beats: 42.95%] - LeetPush

--- 2215-Find-the-Difference-of-Two-Arrays.py
+++ 2215-Find-the-Difference-of-Two-Arrays.py
@@ -3,10 +3,8 @@ class Solution(object):
         set_nums1 = set(nums1)
         set_nums2 = set(nums2)
         
-        # Distinct integers in nums1 not present in nums2
         diff_nums1 = list(set_nums1 - set_nums2)
         
-        # Distinct integers in nums2 not present in nums1
         diff_nums2 = list(set_nums2 - set_nums1)
         
         return [diff_nums1, diff_nums2]
